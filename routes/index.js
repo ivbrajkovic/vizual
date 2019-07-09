e59fd009aa942b89d0b7b06b3fc4cd0abe2f726c
ivbrajkovic <ivan.brajkovic@icloud.com>
2019-07-09 23:51:33 +0200

    v2

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,66 @@
 var express = require('express');
 var router = express.Router();
 var fsPromises = require('fs').promises;
+// var fs = require('fs');
+
+const regex = /-(.*)\./;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // fs.readdir(__dirname + '/../public/images/linkovi/', (err, files) => {
-  // if (err) res.send(`<h2>${err}<h2>`);
-  // else {
-  //   console.log('TCL: files', files);
-  //   res.render('index', { files: files });
-  // }
-  // });
-
+router.get('/test', function(req, res, next) {
   fsPromises
     .readdir(__dirname + '/../public/images/linkovi')
     .then(files => {
-      console.log('TCL: files', files);
-      res.render('index', { files: files });
+      // console.log('TCL: files', files);
+
+      let matches;
+      let obj = {};
+      files.forEach(file => {
+        if ((matches = regex.exec(file)) !== null) {
+          obj[matches[1].toUpperCase().replace(/_/g, ' ')] = file;
+        }
+      });
+
+      res.render('index', { data: obj });
     })
-    .catch(err => res.send(`<h2>${err}<h2>`));
+    // .catch(err => res.send(`<h3 style="color:red;">${err}<h3>`));
+    .catch(error => res.render('error', { error }));
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // let setting = require.resolve('../settings.json');
+  fsPromises
+    .readFile('./settings.json')
+    .then(data => {
+      let settings = JSON.parse(data);
+      res.render('index', { data: settings.kategorija });
+    })
+    .catch(error => res.render('error', { error }));
+});
+
+/* GET gallery page. */
+router.get('/gallery/:kategorija', function(req, res, next) {
+  fsPromises
+    // .readdir(__dirname + '/../public/images/linkovi')
+    .readdir('./public/images/' + req.params.kategorija)
+    .then(files => {
+      console.log('TCL: files', files);
+
+      let obj = {};
+      files.forEach(el => {
+        obj[el.match(/^(.*)\./)[1]] = el;
+      });
+      console.log('TCL: obj', obj);
+
+      res.render('gallery', {
+        data: {
+          dir: req.params.kategorija + '/',
+          src: obj
+        }
+      });
+      // res.send('OK');
+    })
+    .catch(error => res.render('error', { error }));
 });
 
 module.exports = router;
